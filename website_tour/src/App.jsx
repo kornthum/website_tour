eea8e2acb6a047678447a5788788435c03327083
Kornthum Seetanathum <s.kornthum@hotmail.com>
2023-12-18 19:27:17 +0700

    init repo and create pages, routes

---
+++ website_tour/src/App.jsx
@@ -0,0 +1,14 @@
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import Home from "./pages/Home";
+import SignIn from "./pages/SignIn";
+
+export default function App() {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/sign-in" element={<SignIn />} />
+      </Routes>
+    </BrowserRouter>
+  );
+}
